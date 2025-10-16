f8c75818e63a75d383c841c4a529a6e717c0311a
jelile zakariae <jelilezakariae@gmail.com>
2025-10-16 16:18:03 -0700

    156146515

---
+++ task_4.sql
@@ -0,0 +1,12 @@
+SELECT 
+    COLUMN_NAME AS 'Column',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Nullable',
+    COLUMN_DEFAULT AS 'Default',
+    COLUMN_KEY AS 'Key',
+    EXTRA AS 'Extra'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = alx_book_store()  -- the current database passed via mysql command
+    AND TABLE_NAME = 'books'
